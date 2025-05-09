dd88cdbb93ae1dcf692cab60da2e31ed9f8c148e
김경민 <kkm07133@naver.com>
2025-05-09 23:25:02 +0900

    feat: (#1) spring 기본 세팅 추가

---
+++ storties-auth-service/src/main/java/org/example/stortiesauthservice/StortiesAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.stortiesauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StortiesAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StortiesAuthServiceApplication.class, args);
+    }
+
+}
