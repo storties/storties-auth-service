6a5b2548d6d65486112c8e64357980a31ae44241
김경민 <kkm07133@naver.com>
2025-05-12 23:35:09 +0900

    feat: (#2) refreshToken 기능 추가

--- storties-auth-service/src/main/java/storties/auth/stortiesauthservice/authentication/JwtTokenProvider.java
+++ storties-auth-service/src/main/java/storties/auth/stortiesauthservice/authentication/JwtTokenProvider.java
@@ -5,20 +5,30 @@ import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import storties.auth.stortiesauthservice.persistence.type.Role;
-
 import javax.crypto.SecretKey;
+import java.time.Duration;
 import java.util.List;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 토큰 생성 밑 파싱
+ */
 @Component
 public class JwtTokenProvider {
     private final String secret = "secretKeyfHeljfaosdjASDDFeefffHJFTDCVdsaklfjalsdkfjlasdkjfaldkf";
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public JwtTokenProvider(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     /**
      * 토큰 생성 시 Role은 String 형태로 저장됨
@@ -27,11 +37,12 @@ public class JwtTokenProvider {
      * @param role 권한
      * @return 토큰
      */
-    public String createToken(Long id, String userName, Role role) {
+    public String createAccessToken(Long id, String userName, Role role) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", String.valueOf(role));
         claims.put("userName", userName);
         claims.put("id", id);
+        claims.put("tokenType", String.valueOf(Token.ACCESS_TOKEN));
 
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
 
@@ -47,47 +58,108 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public Authentication getAuthentication(String token) {
-        String username = getUsername(token);
+    /**
+     * 리프레시 토큰 생성 및 redis에 저장
+     * @param id 아이디
+     * @return 리프레시 토큰
+     */
+    public String createRefreshToken(Long id) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("tokenType", String.valueOf(Token.REFRESH_TOKEN));
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        String refreshToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 14 * 24 * 60 * 60 * 1000L)) // 임시
+                .signWith(SignatureAlgorithm.HS256, key) // 임시
+                .compact();
+
+        String redisKey = "RT:" + id;
+        redisTemplate.opsForValue().set(redisKey, refreshToken, Duration.ofDays(14));
+
+        return refreshToken;
+    }
+
+    /**
+     * 권한 가져오기
+     * @param accessToken - 엑세스 토큰
+     * @return 권한
+     */
+    public Authentication getAuthentication(String accessToken) {
+        String username = getUsername(accessToken);
         return new UsernamePasswordAuthenticationToken(username, "", List.of());
     }
 
-    public String getUsername(String token) {
+    public String getUsername(String accessToken) {
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
 
         JwtParser parser = Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build();
 
-        return parser.parseClaimsJws(token).getBody().get("userName", String.class);
+        return parser.parseClaimsJws(accessToken).getBody().get("userName", String.class);
     }
 
-    public String getRole(String token) {
+    public String getRole(String accessToken) {
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
 
         JwtParser parser = Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build();
 
-        return parser.parseClaimsJws(token).getBody().get("role", String.class);
+        return parser.parseClaimsJws(accessToken).getBody().get("role", String.class);
     }
 
-    public Long getId(String token) {
+    public Long getId(String accessToken) {
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
 
         JwtParser parser = Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build();
 
-        return parser.parseClaimsJws(token).getBody().get("id", Long.class);
+        return parser.parseClaimsJws(accessToken).getBody().get("id", Long.class);
     }
 
-    public boolean validateToken(String token) {
+    public boolean validateAccessToken(String accessToken) {
         try {
             SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
+
+            JwtParser parser = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build();
+
+            Date expiration = parser.parseClaimsJws(accessToken).getBody().getExpiration();
+            boolean isExpired = expiration.before(new Date());
+
+            if (isExpired) return false;
+
+            String tokenType = parser.parseClaimsJws(accessToken).getBody().get("tokenType", String.class);
+
+            return tokenType.equals("ACCESS_TOKEN");
         } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // todo [401 expired] 추가 해야함
+    public boolean validateRefreshToken(String refreshToken) { // Expired 확인 필요(수정 필요)
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+
+            JwtParser parser = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build();
+
+            Date expiration = parser.parseClaimsJws(refreshToken).getBody().getExpiration();
+            boolean isExpired = expiration.before(new Date());
+
+            if (isExpired) return false;
+
+            String tokenType = parser.parseClaimsJws(refreshToken).getBody().get("tokenType", String.class);
+            return tokenType.equals("REFRESH_TOKEN");
+        } catch (JwtException e) {
             return false;
         }
     }
--- storties-auth-service/src/test/java/storties/auth/stortiesauthservice/authentication/JwtTokenProviderTest.java
+++ storties-auth-service/src/test/java/storties/auth/stortiesauthservice/authentication/JwtTokenProviderTest.java
@@ -2,43 +2,71 @@ package storties.auth.stortiesauthservice.authentication;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import storties.auth.stortiesauthservice.persistence.type.Role;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class JwtTokenProviderTest {
 
-    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider();
+    private RedisTemplate<String, String> mockRedisTemplate;
+    private ValueOperations<String, String> mockValueOperations;
+    private JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        mockRedisTemplate = mock(RedisTemplate.class);
+        mockValueOperations = mock(ValueOperations.class);
+
+        when(mockRedisTemplate.opsForValue()).thenReturn(mockValueOperations);
+
+        jwtTokenProvider = new JwtTokenProvider(mockRedisTemplate);
+    }
 
     @Test
-    @DisplayName("토큰 생성 테스트")
-    void testCreateToken() {
+    @DisplayName("엑세스 토큰 생성 테스트")
+    void testCreateAccessToken() {
         Long id = 1L;
         String userName = "testUser";  // userName 추가
         Role role = Role.ADMIN;
 
-        String token = jwtTokenProvider.createToken(id, userName, role);  // userName 추가
+        String token = jwtTokenProvider.createAccessToken(id, userName, role);  // userName 추가
 
         // 토큰이 정상적으로 생성되었는지 확인 (길이 체크)
         assertNotNull(token);
         assertFalse(token.isEmpty());
 
         // 토큰이 유효한지 검증 (단순 체크)
-        assertTrue(jwtTokenProvider.validateToken(token));
+        assertTrue(jwtTokenProvider.validateAccessToken(token));
     }
 
     @Test
-    @DisplayName("토큰 검사")
+    @DisplayName("리프레시 토큰 생성")
+    void createRefreshToken() {
+        Long id = 1L;
+
+        String token = jwtTokenProvider.createRefreshToken(id);
+
+        assertNotNull(token);
+        assertFalse(token.isEmpty());
+
+        assertTrue(jwtTokenProvider.validateRefreshToken(token));
+    }
+
+    @Test
+    @DisplayName("엑세스 토큰 권한 입증 테스트")
     void testGetAuthentication() {
-        String token = jwtTokenProvider.createToken(1L, "testUser", Role.ADMIN);
+        String token = jwtTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);
 
         Authentication authentication = jwtTokenProvider.getAuthentication(token);
 
@@ -49,9 +77,9 @@ class JwtTokenProviderTest {
     }
 
     @Test
-    @DisplayName("사용자명 추출 테스트")
+    @DisplayName("엑세스 토큰 사용자명 추출 테스트")
     void testGetUsername() {
-        String token = jwtTokenProvider.createToken(1L, "testUser", Role.ADMIN);
+        String token = jwtTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);
 
         String username = jwtTokenProvider.getUsername(token);
 
@@ -61,9 +89,9 @@ class JwtTokenProviderTest {
     }
 
     @Test
-    @DisplayName("ID 추출 테스트")
+    @DisplayName("엑세스 토큰 ID 추출 테스트")
     void testGetId() {
-        String token = jwtTokenProvider.createToken(1L, "testUser", Role.ADMIN);
+        String token = jwtTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);
 
         Long id = jwtTokenProvider.getId(token);
 
@@ -72,9 +100,9 @@ class JwtTokenProviderTest {
     }
 
     @Test
-    @DisplayName("권한 추출 테스트")
+    @DisplayName("엑세스 토큰 Role(권한) 추출 테스트")
     void testGetRole() {
-        String token = jwtTokenProvider.createToken(1L, "testUser", Role.ADMIN);
+        String token = jwtTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);
 
         String role = jwtTokenProvider.getRole(token);
 
@@ -83,30 +111,30 @@ class JwtTokenProviderTest {
     }
 
     @Test
-    @DisplayName("유효한 토큰 테스트")
+    @DisplayName("유효한 엑세스 토큰 테스트")
     void testValidateToken_ValidToken() {
-        String token = jwtTokenProvider.createToken(1L, "testUser", Role.ADMIN);
+        String token = jwtTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);
 
         // 유효한 토큰을 넣었을 때 true가 반환되는지 확인
-        assertTrue(jwtTokenProvider.validateToken(token));
+        assertTrue(jwtTokenProvider.validateAccessToken(token));
     }
 
     @Test
-    @DisplayName("잘못된 토큰 테스트")
+    @DisplayName("잘못된 엑세스 토큰 테스트")
     void testValidateToken_InvalidToken() {
         // 잘못된 토큰을 넣었을 때 false가 반환되는지 확인
         String invalidToken = "invalidToken";
-        assertFalse(jwtTokenProvider.validateToken(invalidToken));
+        assertFalse(jwtTokenProvider.validateAccessToken(invalidToken));
     }
 
     @Test
-    @DisplayName("만료된 토큰 테스트")
+    @DisplayName("만료된 엑세스 토큰 테스트")
     void testValidateToken_ExpiredToken() {
-        // 만료된 토큰을 테스트할 수 있게끔 만료 시간을 조작 (시계 바꾸기 등)
+        // 만료된 토큰을 테스트할 수 있게끔 만료 시간을 조작
         String expiredToken = createExpiredToken();
 
         // 만료된 토큰을 넣었을 때 false가 반환되는지 확인
-        assertFalse(jwtTokenProvider.validateToken(expiredToken));
+        assertFalse(jwtTokenProvider.validateAccessToken(expiredToken));
     }
 
     /**
@@ -117,13 +145,15 @@ class JwtTokenProviderTest {
         long expiredValidity = -1000L; // 현재 시간보다 이전으로 만료 설정
         String secret = "secretFEFDSXXXXXXDFFNMKLOPOHHkeyfkadsjkfjdslkfjladsjflak";
 
-        JwtTokenProvider expiredTokenProvider = new JwtTokenProvider() {
+        JwtTokenProvider expiredTokenProvider = new JwtTokenProvider(mockRedisTemplate) {
+
             @Override
-            public String createToken(Long id, String userName, Role role) {
+            public String createAccessToken(Long id, String userName, Role role) {
                 Map<String, Object> claims = new HashMap<>();
                 claims.put("role", role);
                 claims.put("userName", userName);
                 claims.put("id", id);
+                claims.put("tokenType", Token.ACCESS_TOKEN);
 
                 Date now = new Date();
                 Date exp = new Date(now.getTime() + expiredValidity); // 만료 시간 설정
@@ -136,6 +166,6 @@ class JwtTokenProviderTest {
                         .compact();
             }
         };
-        return expiredTokenProvider.createToken(1L, "testUser", Role.ADMIN);  // userName 추가
+        return expiredTokenProvider.createAccessToken(1L, "testUser", Role.ADMIN);  // userName 추가
     }
 }
