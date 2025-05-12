9fad6ce342a338e3064c8a5b4ce6ad1a32fed7f0
김경민 <kkm07133@naver.com>
2025-05-12 23:35:42 +0900

    feat: (#2) JwtFilter 추가

---
+++ storties-auth-service/src/main/java/storties/auth/stortiesauthservice/authentication/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package storties.auth.stortiesauthservice.authentication;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider provider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider provider) {
+        this.provider = provider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+
+        String token = resolveToken(request);
+        if (token != null && provider.validateAccessToken(token)) {
+            Authentication auth = provider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
+}
