238cd6299eba255a803f608949826e08a714a229
김경민 <kkm07133@naver.com>
2025-05-14 16:16:59 +0900

    feat: (#2) UserEntity 일부 수정

--- storties-auth-service/src/main/java/storties/auth/stortiesauthservice/persistence/User.java
+++ storties-auth-service/src/main/java/storties/auth/stortiesauthservice/persistence/User.java
@@ -5,35 +5,42 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import storties.auth.stortiesauthservice.persistence.type.OauthProvider;
+import storties.auth.stortiesauthservice.persistence.type.AuthProvider;
 import storties.auth.stortiesauthservice.persistence.type.Role;
 import java.time.LocalDateTime;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Builder
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
-    private String name;
-
-    @Column(columnDefinition = "VARCHAR(60)")
+    @Column(columnDefinition = "VARCHAR(60)", nullable = true)
     private String password;
 
-    @Column(columnDefinition = "VARCHAR(20)")
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Column(columnDefinition = "VARCHAR(20)", name = "oauth_provider")
-    private OauthProvider oauthProvider;
+    @Column(columnDefinition = "VARCHAR(20)", name = "auth_provider")
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
 
-    @Column(name = "oauth_provider_id")
+    @Column(name = "oauth_provider_id", nullable = true)
     private String oauthProviderId;
 
     @Column(name = "created_at")
